d409adee87dabe699b999e55ced10a0ff3821b5b
Krzysztof Franek <krzysztof.franek@krzfra.de>
2020-03-01 11:51:39 +0100

    feat: updated imports, removed tests

--- projects/pass-on-encapsulation/src/lib/pass-on-encapsulation.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PassOnEncapsulationDirective } from './pass-on-encapsulation.directive';
-
-describe('PassOnEncapsulationDirective', () => {
-  it('should create an instance', () => {
-    const directive = new PassOnEncapsulationDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- projects/pass-on-encapsulation/src/public-api.ts
+++ projects/pass-on-encapsulation/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of pass-on-encapsulation
  */
 
-export * from './lib/pass-on-encapsulation.service';
-export * from './lib/pass-on-encapsulation.component';
+export * from './lib/pass-on-encapsulation.directive';
 export * from './lib/pass-on-encapsulation.module';
